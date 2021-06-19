8eed9259b401dd0a128f0c1a385ad39a8bc1829a
Student User <student@workstation.lab.example.com>
2021-06-19 11:25:00 -0400

    update

--- hello-java/run-app.sh
+++ hello-java/run-app.sh
@@ -4,4 +4,4 @@ echo "Starting hello-java app..."
 echo "JVM options => $JAVA_OPTIONS"
 echo
 
-java $JAVA_OPTIONS -jar /opt/app-root/bin/hello-java.jar
+java $JAVA_OPTIONS -jar ~/Downloads/hello-java.jar
